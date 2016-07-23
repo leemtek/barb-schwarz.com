2aace60258ab14f9d36383d51b2c32a93a79c92e
Duane Leem <duaneleem@alumni.ggu.edu>
2016-07-23 11:10:43 -0700

    Added customer ng-directive for header.

---
+++ assets/js/angular/directives.js
@@ -0,0 +1,9 @@
+(function() {
+    angular.module("barbschwarz")
+        .directive("siteFooter", [function() {
+            return {
+                templateUrl: "assets/js/angular/directives-template/header.html"
+            }; // return
+        }]) ; // siteFooter
+    ; // angular.module
+})();
