ccd698612cf51c51406e2f3d8ce29690568772a9
Duane Leem <duaneleem@alumni.ggu.edu>
2016-08-03 01:13:12 -0700

    Developed Angular directive for gallery section.  Added image boxes after about section.

--- assets/js/angular/directives.js
+++ assets/js/angular/directives.js
@@ -13,5 +13,12 @@
                 templateUrl: "assets/js/angular/directives-template/footer.html"
             }; // return
         }]) // siteFooter
+
+        .directive("barbsSayings", [function () {
+            return {
+                restrict: 'A',
+                templateUrl: "assets/js/angular/directives-template/barbs-sayings.html"
+            }; // return
+        }]) // siteFooter
     ; // angular.module
 })();
